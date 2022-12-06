1dbe46e3f07e422b9e396a0c597b9e0cc5fc02d8
kobayashi <c0a21051d1@edu.teu.ac.jp>
2022-12-06 14:28:31 +0900

    maze

---
+++ ex03/maze.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("迷えるこうかとん")
+    canvas = tk.Canvas(root,width = 1500,height = 900,bg = "black")
+
+    kokaton = tk.PhotoImage(file="fig/8.png")
+    cx,cy = 300,400
+    canvas.create_image(cx,cy,image = kokaton,tag = "kokaton")
+    canvas.pack()
+    root.mainloop()
